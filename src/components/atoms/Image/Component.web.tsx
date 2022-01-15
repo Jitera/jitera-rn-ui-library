3882406e0fb92b0b33553ddfcb2ed9683129393e
nhat-ndm1193 <nhat.nguyen@iruuza.com>
2022-01-15 09:04:19 +0800

    remove unused type

--- src/components/atoms/Image/Component.web.tsx
+++ src/components/atoms/Image/Component.web.tsx
@@ -1,14 +1,12 @@
 import React, { FC, forwardRef } from 'react';
 import { Image as RnImage, ImageProps as RnImageProps } from 'react-native';
-import type { FastImageProps } from 'react-native-fast-image';
 import type { PropsWithRef } from '../../../type';
 
 export type ImageProps = PropsWithRef<
-  RnImageProps &
-    Omit<FastImageProps, 'children'> & {
-      fastImage?: boolean;
-      uri?: string;
-    }
+  RnImageProps & {
+    fastImage?: boolean;
+    uri?: string;
+  }
 >;
 
 const Image: FC<ImageProps> = forwardRef<any, ImageProps>(
