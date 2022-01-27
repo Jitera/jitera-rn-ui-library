10f9d78431b4fa557265ccf91b0095ad20adedfe
nhat-ndm1193 <nhat.nguyen@iruuza.com>
2022-01-27 11:59:18 +0700

    change style exported component

--- src/components/atoms/Input/ThemedComponent.tsx
+++ src/components/atoms/Input/ThemedComponent.tsx
@@ -5,13 +5,12 @@ import type { RneFunctionComponent } from '../../../theme/helpers';
 const ThemedOTPInput: RneFunctionComponent<Omit<InputProps, 'ref'>> = (
   props
 ) => {
-  const { theme, errorStyle } = props;
+  const { theme, inputStyle } = props;
   return (
     <Input
       {...props}
-      errorStyle={
-        errorStyle || {
-          marginTop: theme?.spacing?.SPACING_5,
+      inputStyle={
+        inputStyle || {
           fontSize: theme?.fontSizes?.FONT_12,
           color: theme?.colors?.error,
         }
