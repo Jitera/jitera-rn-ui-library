db5c49a9ed55db476dd97b0f3da4cfca04797940
Jefry Dewangga <jefrydco@gmail.com>
2022-03-08 10:48:26 +0700

    feat(example): rename project to be unique

--- example/ios/JiteraRNUILibraryExampleTests/JiteraRNUILibraryExampleTests.m
+++ example/ios/JiteraRNUILibraryExampleTests/JiteraRNUILibraryExampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface exampleTests : XCTestCase
+@interface JiteraRNUILibraryExampleTests : XCTestCase
 
 @end
 
-@implementation exampleTests
+@implementation JiteraRNUILibraryExampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
