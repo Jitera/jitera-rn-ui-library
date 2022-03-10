661e17d8e8b336a62dd28f53d603c7208cae2ab2
Jefry Dewangga <jefrydco@gmail.com>
2022-03-10 14:49:57 +0700

    feat(example): add apple login

--- example/src/views/ThirdPartyAuthButton/index.tsx
+++ example/src/views/ThirdPartyAuthButton/index.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
-import { ThirdPartyAuthButton, ThirdPartyAuthProvider, GoogleAuthResult, FacebookAuthResult } from '@jitera/jitera-rn-ui-library'
+import {
+  ThirdPartyAuthButton,
+  ThirdPartyAuthProvider,
+  GoogleAuthResult,
+  FacebookAuthResult,
+  AppleAuthResult
+} from '@jitera/jitera-rn-ui-library'
 
 import BaseLayout from '~/layouts/Base'
+import { AuthSessionResult } from 'expo-auth-session';
 
 const ThirdPartyAuthenticationViews: React.FC = () => {
   const [googleAuthResult, setGoogleAuthResult] = useState<GoogleAuthResult>()
   const [facebookAuthResult, setFacebookAuthResult] = useState<FacebookAuthResult>()
+  const [appleAuthResult, setAppleAuthResult] = useState<AppleAuthResult>()
 
   return (
     <BaseLayout>
@@ -20,7 +28,7 @@ const ThirdPartyAuthenticationViews: React.FC = () => {
         }}
         errorMessage={
           ["cancel", "dismiss", "locked", "error"]
-            .includes(googleAuthResult?.authentication?.type as string) && 'Login Failed'
+            .includes((googleAuthResult?.authentication as Partial<AuthSessionResult>)?.type as string) && 'Login Failed' || undefined
         }
         value={googleAuthResult}
         onChange={(response) => {
@@ -29,6 +37,7 @@ const ThirdPartyAuthenticationViews: React.FC = () => {
         }}
       />
       <ThirdPartyAuthButton
+        style={{marginBottom: 30}}
         type={ThirdPartyAuthProvider.FACEBOOK}
         config={{
           expoClientId: '537504197710261',
@@ -38,11 +47,23 @@ const ThirdPartyAuthenticationViews: React.FC = () => {
         }}
         errorMessage={
           ["cancel", "dismiss", "locked", "error"]
-            .includes(facebookAuthResult?.authentication?.type as string) && 'Login Failed'
+            .includes((facebookAuthResult?.authentication as Partial<AuthSessionResult>)?.type as string) && 'Login Failed' || undefined
         }
         value={facebookAuthResult}
         onChange={(response) => {
           setFacebookAuthResult(response as FacebookAuthResult)
+          console.log(response)
+        }}
+      />
+      <ThirdPartyAuthButton
+        type={ThirdPartyAuthProvider.APPLE}
+        errorMessage={
+          ["cancel", "dismiss", "locked", "error"]
+            .includes((appleAuthResult?.authentication as Partial<AuthSessionResult>)?.type as string) && 'Login Failed' || undefined
+        }
+        value={appleAuthResult}
+        onChange={(response) => {
+          setAppleAuthResult(response as AppleAuthResult)
           console.log(response)
         }}
       />
