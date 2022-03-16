0c15bf36b68e9da315e9d898b592230bc99d5594
Nguyễn Dương <duong.nguyen@jitera.com>
2022-03-16 16:19:58 +0700

    fix: webview

--- src/components/atoms/WebView/Component.tsx
+++ src/components/atoms/WebView/Component.tsx
@@ -1,28 +1,47 @@
 import React, { forwardRef } from 'react';
-
-import type { WebView as RNWebView, WebViewProps as RNWebViewProps } from 'react-native-webview';
+import type { StyleProp, ViewStyle } from 'react-native';
+import type {
+  WebView as RNWebViewType,
+  WebViewProps as RNWebViewProps,
+} from 'react-native-webview';
+import { WebView as RNWebView } from 'react-native-webview';
 import type { WebViewSource } from 'react-native-webview/lib/WebViewTypes';
 import type { PropsWithRef } from '../../../type';
 
 export type WebViewProps = PropsWithRef<
   {
-    Component: typeof RNWebView;
-  } & Omit<RNWebViewProps, 'source'>
-  & {
-    source?: string | WebViewSource
-  }
+    Component: typeof RNWebViewType;
+  } & Omit<RNWebViewProps, 'source'> & {
+      style?: StyleProp<ViewStyle>;
+      isPreview?: boolean;
+      source?: string | WebViewSource;
+    }
 >;
 
-const WebView: React.FC<WebViewProps> = forwardRef<RNWebView, WebViewProps>(
-  ({ Component, source, ...props }, ref) => {
-  let newSource = source
+const WebView: React.FC<WebViewProps> = forwardRef<any, WebViewProps>(
+  ({ Component, style, source, isPreview = false, ...props }, ref) => {
+    let newSource = source;
 
-  if (typeof source === 'string') {
-    newSource = {uri: source} 
+    if (typeof source === 'string') {
+      newSource = { uri: source };
+    }
+
+    let WebViewComponent = RNWebView;
+
+    if (isPreview) {
+      WebViewComponent = Component;
+    }
+
+    return (
+      <WebViewComponent
+        ref={ref}
+        containerStyle={style} // for react-native-web-view
+        style={style} // for html preview
+        {...props}
+        source={newSource as WebViewSource}
+      />
+    );
   }
-
-  return <Component ref={ref} {...props} source={newSource as WebViewSource} />
-}
 );
 
 WebView.displayName = 'WebView';
