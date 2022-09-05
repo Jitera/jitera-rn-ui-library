a335b4fa9e4538909f2fae5131a77a1fe67430f3
Luan Nguyen <luan.nguyen@jitera.com>
2022-09-05 21:41:14 +0700

    Support svg image (cont)

--- src/components/atoms/Image/Component.tsx
+++ src/components/atoms/Image/Component.tsx
@@ -1,18 +1,19 @@
 import React, { useMemo } from "react";
-import { Image as RnImage, ImageProps as RnImageProps } from "react-native";
+import { Image as RnImage, ImageStyle, ImageProps as RnImageProps } from "react-native";
 import type { PropsWithRef } from "../../../type";
 
 export type ImageProps = PropsWithRef<
   Partial<RnImageProps> & {
       ImageComponent?: React.ElementType<RnImageProps | any>;
       uri?: string;
-      source: RnImageProps["source"] | string | React.ElementType<RnImageProps | any>;
+      source: RnImageProps["source"] | string | React.ElementType<ImageStyle | any>;
       isSVGImage?: boolean;
+      style: ImageStyle;
     }
 >;
 
 const Image = React.forwardRef<RnImage, ImageProps>(
-  ({ resizeMode = "cover", source, isSVGImage= false, ImageComponent = RnImage, uri, style, ...props }, ref) => {
+  ({ resizeMode = "cover", source, isSVGImage = false, ImageComponent = RnImage, uri, style, ...props }, ref) => {
     const newSource = useMemo(() => {
       if (isSVGImage) return;
       if (uri) {
@@ -27,13 +28,13 @@ const Image = React.forwardRef<RnImage, ImageProps>(
     }, [uri, source, isSVGImage]);
 
     if (isSVGImage) {
-      const Source = source as React.ElementType<RnImageProps | any> ;
+      const Source = source as React.ElementType<ImageStyle | any> ;
 
       return <Source {...props} width={style?.width} height={style?.height} style={style} />;
     }
 
     return (
-      <ImageComponent {...props} ref={ref} source={newSource} resizeMode={resizeMode} />
+      <ImageComponent {...props} ref={ref} source={newSource} style={style} resizeMode={resizeMode} />
     );
   }
 );
